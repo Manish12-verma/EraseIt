609d07077f978cf89b0ff45af3a6e8433f18956d
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2025-07-04 21:37:32 +0530

    backend setup completed

---
+++ Server/server.js
@@ -0,0 +1,23 @@
+import 'dotenv/config'
+import express from 'express';
+import cors from 'cors';
+import connectDB from './configs/mongodb.js';
+
+//App config
+const PORT = process.env.PORT || 5000;
+const app = express();
+await connectDB();
+ 
+
+//Middleware
+app.use(express.json())
+app.use(cors());
+
+
+//API 
+app.get('/',(req,res)=>res.send('API is running'));
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
