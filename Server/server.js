f75325306391c5c4e44610b91cf5930a01834367
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2025-07-06 00:25:40 +0530

    updated server

--- Server/server.js
+++ Server/server.js
@@ -22,5 +22,9 @@ app.use(cors());
 app.get('/', (req, res) => res.send('Hello from EraseIt Server'));
 app.use('/api/user',userRouter)
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
- 
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+export default app;
