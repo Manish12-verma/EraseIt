09ac6250499ca19b076c9926f4c3cf95aa140731
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2025-07-05 20:02:55 +0530

    added vercelJSON

---
+++ Server/controllers/userController.js
@@ -0,0 +1,13 @@
+import userModel from "../models/userModel.js";
+
+//API controller functions to manage clerk user with database
+//http://localhost:4000/api/user/webhooks
+
+const webhooks = async (req, res) => {
+
+     
+
+}
+
+
+
